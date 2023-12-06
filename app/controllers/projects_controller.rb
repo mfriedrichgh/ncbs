915456fb06924d6584f30142fdfbace7eda2a163
Marvin Friedrich <marvin.friedrich@suse.com>
2023-12-06 12:01:15 +0100

    Add basic features

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,10 @@
+class ProjectsController < ApplicationController
+    def index
+
+    end
+
+    # Show a specific project
+    def show
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+    resources :projects
+  
+    # Defines the root path route ("/")
+    root "projects#index"
+  
+    # Users
+    get "/users" => "users#index"
+    get "/:userid" => "users#show"
+    get "/:userid/:projectid" => "projects#show"
+    get "/projects" => "projects#index"
 end
